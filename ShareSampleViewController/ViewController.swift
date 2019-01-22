80c2c1b0df6c0a206185ddfba49946d1e5aaeb52
OrangeApps Inc <destrezajojo@gmail.com>
2019-01-22 17:09:45 +0800

    Initial Commit

---
+++ ShareSampleViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareSampleViewController
+//
+//  Created by OrangeApps Inc. on 1/22/19.
+//  Copyright © 2019 OrangeApps Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
